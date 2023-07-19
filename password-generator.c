93d5970a99061c0513886a2491ef5663f36e81bf
Ankit Raj <135986902+ankitraj5670@users.noreply.github.com>
2023-07-19 17:28:46 +0530

    Add files via upload

---
+++ password-generator.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+#include <math.h>
+
+#define max 21
+
+void generate_password(char password[], int strength, int length) {
+    char letter[] = "abcdefghijklmnopqrstuvwxyz";
+    char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    char num[] = "0123456789";
+    char symbol[] = "!@#$%^&*_+:<|?>/.";
+
+    if (strength == 1) {
+        for (int i = 0; i < length; i++) {
+            password[i] = letter[rand() % 26];
+        }
+        password[length] = '\0';
+    }
+    else if (strength == 2) {
+        for (int i = 0; i < length; i++) {
+            int random_generator = rand() % 2;
+            if (random_generator == 0) {
+                password[i] = letter[rand() % 26];
+            }
+            else {
+                password[i] = LETTER[rand() % 26];
+            }
+        }
+        password[rand() % length] = letter[rand() % 26];
+        password[rand() % length] = LETTER[rand() % 26];
+        password[length] = '\0';
+    }
+    else if (strength == 3) {
+        for (int i = 0; i < length; i++) {
+            int random_generator = rand() % 3;
+            if (random_generator == 0) {
+                password[i] = letter[rand() % 26];
+            }
+            else if (random_generator == 1) {
+                password[i] = LETTER[rand() % 26];
+            }
+            else {
+                password[i] = num[rand() % 10];
+            }
+        }
+        password[rand() % length] = letter[rand() % 26];
+        password[rand() % length] = LETTER[rand() % 26];
+        password[rand() % length] = num[rand() % 10];
+        password[length] = '\0';
+    }
+    else if (strength == 4) {
+        for (int i = 0; i < length; i++) {
+            int random_generator = rand() % 4;
+            if (random_generator == 0) {
+                password[i] = letter[rand() % 26];
+            }
+            else if (random_generator == 1) {
+                password[i] = LETTER[rand() % 26];
+            }
+            else if (random_generator == 2) {
+                password[i] = num[rand() % 10];
+            }
+            else {
+                password[i] = symbol[rand() % 17];
+            }
+        }
+        password[rand() % length] = letter[rand() % 26];
+        password[rand() % length] = LETTER[rand() % 26];
+        password[rand() % length] = num[rand() % 10];
+        password[rand() % length] = symbol[rand() % 17];
+        password[length] = '\0';
+    }
+}
+
+bool has_ambiguous_characters(const char password[]) {
+    const char ambiguous[] = {'l', '1', 'I', 'o', '0', 'O'};
+    const int password_length = strlen(password);
+
+    for (int i = 0; i < password_length; i++) {
+        for (int j = 0; j < sizeof(ambiguous) / sizeof(char); j++) {
+            if (password[i] == ambiguous[j]) {
+                return true;
+            }
+        }
+    }
+
+    return false;
+}
+
+float evaluate_password(const char password[]) {
+    int len = strlen(password);
+    int character_count = 0;
+    int uppercase_count = 0;
+    int lowercase_count = 0;
+    int digit_count = 0;
+    int symbol_count = 0;
+
+    for (int i = 0; i < len; i++) {
+        char c = password[i];
+        if (c >= 'a' && c <= 'z') {
+            lowercase_count++;
+        }
+        else if (c >= 'A' && c <= 'Z') {
+            uppercase_count++;
+        }
+        else if (c >= '0' && c <= '9') {
+            digit_count++;
+        }
+        else {
+            symbol_count++;
+        }
+        character_count++;
+    }
+
+    float entropy = log2(pow(26, lowercase_count) * pow(26, uppercase_count) * pow(10, digit_count) * pow(17, symbol_count));
+
+    float percent = (entropy / (log2(pow(26, character_count)))) * 100;
+
+    return percent;
+}
+
+int main() {
+    srand(time(NULL));
+    int strength, length;
+    char password[max];
+
+    printf("=====================================\n");
+    printf("         PASSWORD GENERATOR          \n");
+    printf("=====================================\n\n");
+
+    printf("1: Weak\n2: Moderate\n3: Strong\n4: Very Strong\n");
+    printf("Select the desired strength (1-4): ");
+    scanf("%d", &strength);
+
+    printf("Select the desired length (6-20): ");
+    scanf("%d", &length);
+
+    if (strength < 1 || strength > 4 || length < 6 || length > 20) {
+        printf("\nInvalid input!\n");
+        exit(0);
+    }
+
+    while (1) {
+        generate_password(password, strength, length);
+
+        system("cls");
+
+        printf("\n=====================================\n");
+        printf("         GENERATED PASSWORD          \n");
+        printf("=====================================\n");
+
+        printf("\n%s\n", password);
+
+        if (has_ambiguous_characters(password)) {
+            printf("\nNote: The password contains ambiguous characters.\n");
+        }
+
+        float percent = evaluate_password(password);
+        printf("\nPassword strength: %.1f%%\n", percent);
+
+        printf("\n=====================================\n");
+        printf("            TRY AGAIN?               \n");
+        printf("=====================================\n");
+
+        printf("\n1: YES\n2: NO\n\nSelect an option: ");
+        int again;
+        scanf("%d", &again);
+
+        if (again == 2) {
+            break;
+        }
+
+        system("cls");
+    }
+
+    printf("\n=====================================\n");
+    printf("          THANK YOU FOR USING         \n");
+    printf("          PASSWORD GENERATOR!           \n");
+    printf("=====================================\n");
+
+    return 0;
+}
